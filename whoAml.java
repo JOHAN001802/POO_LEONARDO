fc4afbc4f22ac45189d528fe57a78c6112389d1e
JOHAN001802 <144969252+JOHAN001802@users.noreply.github.com>
2024-04-23 18:05:43 -0500

    Ejercicios de practica
    Se anexa ejercicio TALLER 5

---
+++ whoAml.java
@@ -0,0 +1,28 @@
+package taller5;
+
+public class whoAml {
+    /*
+¿quién soy?
+Un grupo de componentes de Java, disfrazados, están jugando al juego de mesa "¿Quién soy yo?" te dan una pista,
+e intentas adivinar quiénes son, basándose en lo que dicen. Supongamos que siempre dicen la verdad sobre sí mismos.
+Si dicen algo que podría ser cierto para más de uno de ellos, elija todos para quienes esa frase.
+Puede aplicar. Complete los espacios en blanco al lado de la oración con los nombres de uno o más asistentes. El primero corre por nuestra cuenta
+Los asistentes de esta noche:
+Clase.
+Método.
+Objeto.
+Los valores de mis variables de instancia pueden ser diferentes de los de mi amigo = Objeto, Variable de instancia
+Me comporto como una plantilla = Clase
+Me gusta hacer cosas = Método
+Puedo tener muchos métodos = Clase, Objeto
+Represento 'estado' = Objeto, variable de instancia
+Tengo comportamientos = Método
+Estoy ubicado en objetos = Variable de instancia
+Vivo en el montón = Objeto, variable de instancia
+Estoy acostumbrado a crear instancias de objetos = Clase
+Mi estado puede cambiar = Objeto, variable de instancia
+Declaro métodos = Clase
+Puedo cambiar en tiempo de ejecución = Objeto, variable de instancia
+*/
+}
+
