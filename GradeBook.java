1169c2895a5dc117f362568b94236dda88600ac7
JOHAN001802 <144969252+JOHAN001802@users.noreply.github.com>
2024-04-23 17:19:32 -0500

    Calculando el promedio de la clase GradeBook
    Se anexa taller3 :Calculando el promedio de la clase GradeBook

--- GradeBook.java
+++ GradeBook.java
@@ -1,4 +1,4 @@
-package taller1;
+package taller3;
 
 public class GradeBook {
 
@@ -6,6 +6,12 @@ public class GradeBook {
 
     public GradeBook() {
     }
+    public static long promEstudiante(long a, long b, long c, long d, long e){
+        long prom = (a+b+c+d+e)/5;
+
+        return prom;
+    };
+
 
     public GradeBook(String cs102DataStructuresInJava) {
     }
